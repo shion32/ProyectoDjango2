7a1db16234fed695a41658bd1ab5e5e542a46cc5
shion32 <annysofiamontano@gmail.com>
2025-04-02 18:27:24 -0500

    Guia 6

--- todo/models.py
+++ todo/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-class Tareas(models.Model):
+class Tarea(models.Model):
     tarea=models.CharField(max_length=100)
     def __str__(self):
         return self.tarea
--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+
 from todo import views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('', views.home,name="home"),
-
+    path('admin/', admin.site.urls),
+    path("", views.home,name="home"),
+    path("agregar/", views.agregar, name="agregar"),
+    path("eliminar/<int:tarea_id>",views.eliminar,name="eliminar"),
+    path("editar/<int:tarea_id>",views.editar,name="editar"),
 ]
